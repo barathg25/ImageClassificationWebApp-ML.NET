94d9e73b88e2c0c03a935981b2c5ad542c3f5d6e
bar <bar@gmail.com>
2025-04-18 22:16:04 +0530

    my commit

--- WebApp.Predict/Pages/Error.cshtml.cs
+++ WebApp.Predict/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace WebApp.Predict.Pages
+namespace MvcMovie.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
--- WebApp.Predict/Pages/Index.cshtml.cs
+++ WebApp.Predict/Pages/Index.cshtml.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace WebApp.Predict.Pages
+namespace MvcMovie.Pages
 {
     public class IndexModel : PageModel
     {
